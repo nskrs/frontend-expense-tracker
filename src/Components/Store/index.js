a9f58687c04b3d4765e3b716a0c6471b69e072d0
nskrs <nitinkolhe051>
2023-04-22 20:10:26 +0530

    Shoping page app using reducers

--- src/Components/Store/index.js
+++ src/Components/Store/index.js
@@ -1,63 +1,44 @@
+const { configureStore, createSlice } = require('@reduxjs/toolkit');
 
-import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const inititalExpenseState = { expenses: [],totalExpense:0 }
-const expenseSlice = createSlice({
-    name: "expense",
-    initialState: inititalExpenseState,
+const initialCartState = { cartItems: [], cardShow: false }
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: initialCartState,
     reducers: {
-        setExpenses(state,action) {
-            state.expenses=action.payload;
+        setToggle(state, actions) {
+            state.cardShow = actions.payload
         },
-        setTotalExpense(state,action) {
-            state.expenses=action.payload;
+        setAddCartItem(state, actions) {
+            state.cartItems = [...state.cartItems, actions.payload]
+        },
+        setCartItemIncrease(state, actions) {
+            state.cartItems.map((item) => {
+                if (item.prodId === actions.payload) {
+                    item.quantity = item.quantity + 1;
+                }
+                return true;
+            })
+        },
+        setCartItemDecrease(state, actions) {
+            state.cartItems.map((item) => {
+                if (item.prodId === actions.payload) {
+                    item.quantity = item.quantity - 1;
+                }
+                return true;
+            })
+        }, setCartFilter(state) {
+            state.cartItems= state.cartItems.filter((item) => {
+                return item.quantity > 0
+            })
         }
     }
 })
-const inititalThemeState = { mode:false }
-const themeSlice = createSlice({
-    name: "theme",
-    initialState: inititalThemeState,
-    reducers: {
-        setMode(state) {
-            state.mode=!state.mode;
-        }
-    }
-})
 
-const inititalAuthState = {
-    login: false,
-    token: localStorage.getItem("token"),
-    isLoggedIn: localStorage.getItem("token"),
-    profileInfo:{ myName: "", myUrl: "" }
-}
-const authSlice = createSlice({
-    name: "authentication",
-    initialState: inititalAuthState,
-    reducers: {
-        loginTrue(state) {
-            state.login = true;
-        },
-        loginFalse(state) {
-            state.login = false;
-        },
-        setToken(state, action) {
-            state.token = action.payload;
-        },
-        setIsloggedIn(state,action){
-            state.isLoggedIn=action.payload;
-        },
-        setProfileInfo(state,action){
-            state.profileInfo=action.payload;
-        }
-    }
-})
 
 const store = configureStore({
-    reducer: { expense: expenseSlice.reducer, authentication: authSlice.reducer, theme: themeSlice.reducer }
-});
+    reducer: { cart: cartSlice.reducer }
+})
 
 export default store;
-export const expenseActions = expenseSlice.actions;
-export const authActions = authSlice.actions;
-export const themeActions = themeSlice.actions;
+export const cartActions = cartSlice.actions;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './Components/Store/index';
+import store from './components/Store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>);
+root.render(<Provider store={store}><App /></Provider>);
